7cecf18be28931c66b07a7443fd65830600a8da3
Jelly <ccwen815@gmail.com>
2018-11-05 08:06:00 +0800

    [continue][高效率資料分析]ch1 p.32

--- first_script.py
+++ first_script.py
@@ -220,3 +220,24 @@ print("Output #91: {}".format(my_lists_sorted_by_index_3))
 my_lists = [[123,2,2,444],[22,6,6,444],[354,4,4,678],[236,5,5,678],[578,1,1,290],[641,1,1,290]]
 my_lists_sorted_by_index_3_and_0 = sorted(my_lists,key=itemgetter(3,0))
 print("Output #92: {}".format(my_lists_sorted_by_index_3_and_0)) 
+
+#############tuple#############
+my_tuple = ('x','y','z')
+print("Output #93 : {}".format(my_tuple))
+print("Output #94 : my_tuple has {} elements".format(len(my_tuple)))
+print("Output #95 : {}".format(my_tuple[1]))
+longer_tuple = my_tuple + my_tuple
+print("Output #96: {}".format(longer_tuple))
+      
+one,two,three = my_tuple
+print("Output #97: {0} {1} {2}".format(one,two,three))
+var1='red'
+var2='robin'
+print("Output #98:{} {}".format(var1,var2))
+var1,var2 = var2,var1
+print("Output #99:{} {}".format(var1,var2))
+      
+my_list = [1,2,3]
+my_tuple = ('x','y','z')
+print("Output #100: {}".format(tuple(my_list)))
+print("Output #101: {}".format(list(my_tuple)))
