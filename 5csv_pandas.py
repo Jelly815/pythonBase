8dbfb8a1a4d8147256f884b1b4b7c3618c2b05ce
Jelly <ccwen815@gmail.com>
2018-11-15 23:44:33 +0800

    [continue][高效率資料分析]ch2 p.99

--- 5csv_pandas.py
+++ 5csv_pandas.py
@@ -6,5 +6,5 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 data_frame = pd.read_csv(input_file,error_bad_lines=False)
 
-data_frame_value = data_frame.loc[data_frame['Invoice Number'].str.startswith("001-"), :]
+data_frame_value = data_frame.iloc[:,[0,3]]
 data_frame_value.to_csv(output_file, index=False)
